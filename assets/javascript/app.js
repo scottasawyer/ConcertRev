f2e87076c01aaecf3434a83db7b30ff690d46d2b
Scott Sawyer <38329255+scottasawyer@users.noreply.github.com>
2018-08-05 21:26:43 -0400

    a whole lot of javascript but the weather works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,8 +3,7 @@ $(document).ready(function () {
     $('#goingBack').hide();
     var showList = [];
     var savedConcerts = [];
-    var $latitude = 0;
-    var $longitude = 0;
+    var eventCoords = [];
 
     var config = {
         apiKey: "AIzaSyDwVaReBYZNKqN2NV_5zyik1Huff2ka_ws",
@@ -33,14 +32,12 @@ $(document).ready(function () {
 
 
         var queryURL = "https://rest.bandsintown.com/artists/" + userGuess + "/events?app_id=ac5d152e5bb85a812504fe7f16f5ff23";
-        // console.log(queryURL);
 
         $.ajax({
             url: queryURL,
             method: "GET",
 
         }).then(function (response) {
-            // console.log(response)
             showList = [];
 
             var countries = [];
@@ -75,30 +72,36 @@ $(document).ready(function () {
                 goodResults.push(showList[i])
             }
         }
-        // console.log(goodResults);
 
         for (var l = 0; l < goodResults.length; l++) {
             var $div = $('<div>')
             $div.addClass('show-details')
 
             var _starGif = $('<button type="button" class="starConcert"><i class="fas fa-star"></i></button>')
+            // var wButton = $('<p><a class="waves-effect waves-light btn weatherShow"><i class="fas fa-sun"></i> See the Weather</a>')
 
             var $description = $('<p>')
             $description.text('Concert Information: ' + goodResults[l].description)
             var $lineUp = $('<p>')
             $lineUp.text('Lineup: ' + goodResults[l].lineup)
             var $dateTime = $('<p id="wDate">')
-            var wDate = moment(goodResults[l].datetime).format('MMMM Do YYYY, h:mm a');
-            $dateTime.text('Date: ' + wDate);
+            var eDate = moment(goodResults[l].datetime).format('MMMM Do YYYY, h:mm a');
+            $dateTime.text('Date: ' + eDate);
             var $venueName = $('<p>')
-            $venueName.text('Venue: ' + goodResults[l].venue.name)
-            var $region = $('<p>')
-            $region.text('Region: ' + goodResults[l].venue.city)
-            var $latitude = $('<p>')
-            $latitude.text('Latitude: ' + goodResults[l].venue.latitude)
-            var $longitude = $('<p>')
-            $longitude.text('Latitude: ' + goodResults[l].venue.longitude)
+            $venueName.text('Venue: ' + goodResults[l].venue.name + " in " + goodResults[l].venue.city)
+            var $eventInfo = $('<p>')
+            // var ticketCheck = goodResults[l].offers[0].status;
+            // if (goodResults[l].offers[0].status === 'available') {
+            //     $eventInfo.html('<a href="' + goodResults[l].offers[0].url + '" target="blank"> Get tickets here!')
+            // } else {
+            //     $eventInfo.text('Tickets not currently available.')
+            // }
 
+            var $venueWeather = $('<p class="weather">')
+            $venueWeather.text("Click here to see the weather!")
+                .attr('data-latitude', goodResults[l].venue.latitude)
+                .attr('data-longitude', goodResults[l].venue.longitude)
+            console.log("got here");
             _starGif.attr('data-description', goodResults[l].description)
                 .attr('data-lineup', goodResults[l].lineup)
                 .attr('data-dateTime', goodResults[l].datetime)
@@ -106,20 +109,23 @@ $(document).ready(function () {
                 .attr('data-region', goodResults[l].venue.city)
                 .attr('data-latitude', goodResults[l].venue.latitude)
                 .attr('data-longitude', goodResults[l].venue.longitude)
-
-            $div.append($description, $lineUp, $dateTime, $venueName, $region, $latitude, $longitude, _starGif)
+            console.log("Latitude: " + goodResults[l].venue.latitude + ", Longitude: " + goodResults[l].venue.longitude);
+            $div.append($description, $lineUp, $dateTime, $venueName, $venueWeather, _starGif)
             $('#goodResults').append($div)
+
         }
     })
-
-    // if (!Array.isArray(savedConcerts)) {
-    //     savedConcerts = [];
-    // } 
 
     $(document).on('click', '.starConcert', function () {
         $(this).remove();
         console.log("clicked");
-        favConcert = { 'data-description': $(this).attr('data-description'), 'data-lineup': $(this).attr('data-lineup'), 'data-dateTime': $(this).attr('data-dateTime'), 'data-venue': $(this).attr('data-venue'), 'data-region': $(this).attr('data-region') }
+        favConcert = {
+            'data-description': $(this).attr('data-description'),
+            'data-lineup': $(this).attr('data-lineup'),
+            'data-dateTime': $(this).attr('data-dateTime'),
+            'data-venue': $(this).attr('data-venue'),
+            'data-region': $(this).attr('data-region')
+        }
         console.log($(this).attr('data-description'))
         savedConcerts.push(favConcert)
         console.log("HELLO" + favConcert)
@@ -179,6 +185,40 @@ $(document).ready(function () {
         $('#countryList').show(1000);
         $('#goodResults').show();
         $('#goingBack').hide();
+    })
+
+    $(document).on('click', '.weather', function () {
+        console.log("clicked");
+        console.log(this);
+        var wLat = $(this).attr('data-latitude');
+        var wLong = $(this).attr('data-longitude');
+        var wCoords = (wLat + "," + wLong);
+        console.log(wCoords);
+        var wKey = "8c6957ee01f24957b5bd52d69928bd75";
+        var weatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + wLat + "&lon=" + wLong + "&appid=" + wKey + "&units=imperial";
+        $.ajax({
+            url: weatherURL,
+            method: "GET",
+        }).then(function (response) {
+            var vTempHigh = response.main.temp_max;
+            console.log(vTempHigh);
+            var vTempLow = response.main.temp_min;
+            console.log(vTempLow);
+            sessionStorage.setItem('highTemp', vTempHigh);
+            sessionStorage.setItem('lowTemp', vTempLow)
+        })
+        var vTempHigh = sessionStorage.getItem('highTemp');
+        var vTempLow = sessionStorage.getItem('lowTemp');
+        // setTimeout(function(){
+            $(this).text("Today's Forecast: High of " + vTempHigh + " and low of " + vTempLow + " (F)");
+            console.log(this);
+            console.log("got here");
+        // }, 500);
+            
+
+        // console.log("High Temp= " + vTempHigh + " and Low Temp = " + vTempLow);
+        // $(this).html("<p>Today's Forecast: High of " + vTempHigh + " and low of " + vTempLow + " (F)")
+
     })
 
     // $(document).on("click", "#favorites", concertData);
