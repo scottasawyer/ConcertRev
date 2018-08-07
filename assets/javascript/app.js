536b95a4e78c98b431f80537ad02719344f6e13f
scottasawyer <38329255+scottasawyer@users.noreply.github.com>
2018-08-06 21:00:26 -0400

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,10 +27,13 @@ $(document).ready(function () {
         $('#goodResults').hide();
 
         var userGuess = $('.whatDoYouGot').val().trim();
+        console.log(userGuess);
 
-        database.ref().push({userGuess});
+        database.ref().set({ 
+            Band: userGuess 
+        });
         // this might not work, but it should
-        
+
         var queryURL = "https://rest.bandsintown.com/artists/" + userGuess + "/events?app_id=ac5d152e5bb85a812504fe7f16f5ff23";
         // console.log(queryURL);
 
@@ -53,7 +56,7 @@ $(document).ready(function () {
             console.log(showList);
             for (var k = 0; k < countries.length; k++) {
                 var $btn = $('<button>')
-                $btn.attr({'data-country': countries[k], 'id' : "animateImage" + k})
+                $btn.attr({ 'data-country': countries[k], 'id': "animateImage" + k })
                 $btn.text(countries[k])
                 $btn.addClass('country-btn animated rubberBand')
                 $('#countryList').append($btn)
@@ -62,9 +65,9 @@ $(document).ready(function () {
     })
 
     $(document).on('click', '.country-btn', function () {
-        
+
         $('.country-btn').not(this).toggle(1000);
-            
+
         $('#goodResults').show();
         $('#goodResults').empty();
         event.preventDefault();             // prevents refresh
@@ -82,7 +85,7 @@ $(document).ready(function () {
             $div.addClass('show-details')
 
             var _starGif = $('<button type="button" class="starConcert"><i class="fas fa-star"></i></button>')
-            
+
             var $description = $('<p>')
             $description.text('Concert Information: ' + goodResults[l].description)
             var $lineUp = $('<p>')
@@ -94,94 +97,94 @@ $(document).ready(function () {
             $venueName.text('Venue: ' + goodResults[l].venue.name + " in " + goodResults[l].venue.city)
 
 
-            var $venueWeather = $('<p class="weather">')
+            var $venueWeather = $('<p><i class="fa fa-cloud"></i>')
             $venueWeather.text("Click here to see the weather!")
-            .attr('data-latitude', goodResults[l].venue.latitude)
-            .attr('data-longitude', goodResults[l].venue.longitude)
-            console.log("got here");
+                .attr('data-latitude', goodResults[l].venue.latitude)
+                .attr('data-longitude', goodResults[l].venue.longitude)
+            // console.log("got here");
             _starGif.attr('data-description', goodResults[l].description)
-            .attr('data-lineup', goodResults[l].lineup)
-            .attr('data-dateTime', goodResults[l].datetime)
-            .attr('data-venue', goodResults[l].venue.name)
-            .attr('data-region', goodResults[l].venue.city)
-            .attr('data-latitude', goodResults[l].venue.latitude)
-            .attr('data-longitude', goodResults[l].venue.longitude)
+                .attr('data-lineup', goodResults[l].lineup)
+                .attr('data-dateTime', goodResults[l].datetime)
+                .attr('data-venue', goodResults[l].venue.name)
+                .attr('data-region', goodResults[l].venue.city)
+                .attr('data-latitude', goodResults[l].venue.latitude)
+                .attr('data-longitude', goodResults[l].venue.longitude)
             console.log("Latitude: " + goodResults[l].venue.latitude + ", Longitude: " + goodResults[l].venue.longitude);
             $div.append($description, $lineUp, $dateTime, $venueName, $venueWeather, _starGif)
             $('#goodResults').append($div)
-            }
+        }
     })
-     
+
     if (!Array.isArray(savedConcerts)) {
         savedConcerts = [];
-    } 
+    }
 
-    $(document).on('click', '.starConcert', function() {
-        
-        favConcert = {'data-description': $(this).attr('data-description'), 'data-lineup': $(this).attr('data-lineup'), 'data-dateTime': $(this).attr('data-dateTime'), 'data-venue': $(this).attr('data-venue'), 'data-region': $(this).attr('data-region')}
+    $(document).on('click', '.starConcert', function () {
+
+        favConcert = { 'data-description': $(this).attr('data-description'), 'data-lineup': $(this).attr('data-lineup'), 'data-dateTime': $(this).attr('data-dateTime'), 'data-venue': $(this).attr('data-venue'), 'data-region': $(this).attr('data-region') }
         savedConcerts.push(favConcert)
         localStorage.setItem('savedConcerts', JSON.stringify(savedConcerts))
     })
 
-    $('#favorites').on('click', function(event) {
-        
+    $('#favorites').on('click', function (event) {
+
         console.log("clicked");
         $('#goingBack').show();
         $('#concertInformation').slideToggle(1000);
         $('#concertInformation').empty();
         $('#countryList').hide(1000);
         $('#goodResults').hide(1000);
-        
+
         event.preventDefault();
         var savedConcerts = JSON.parse(localStorage.getItem('savedConcerts'));
         console.log(savedConcerts)
 
-        
 
-        for(var m = 0; m < savedConcerts.length; m++) {
+
+        for (var m = 0; m < savedConcerts.length; m++) {
             console.log(savedConcerts[m]['data-description'])
             console.log(m)
             var concertDiv = $('<div>').attr('id', m).addClass('thisClass')
 
             var btnConcert = $('<button>')
-            
+
             btnConcert.addClass('removeConcert').html('<i class="fas fa-trash-alt"></i>').attr('id', "m" + m)
 
-        Object.keys(savedConcerts[m]).forEach(key => {
-            
-            console.log(key);
-            console.log(savedConcerts[m][key])
+            Object.keys(savedConcerts[m]).forEach(key => {
 
-            var concertInfo = $('<p>').addClass('_paragraph')                                                        
-                                      .text((savedConcerts[m][key]))
-                                      
-            concertDiv.append(concertInfo)
-        })
-        concertDiv.append(btnConcert)
-        $('#concertInformation').append(concertDiv)
+                console.log(key);
+                console.log(savedConcerts[m][key])
+
+                var concertInfo = $('<p>').addClass('_paragraph')
+                    .text((savedConcerts[m][key]))
+
+                concertDiv.append(concertInfo)
+            })
+            concertDiv.append(btnConcert)
+            $('#concertInformation').append(concertDiv)
         }
     })
 
-    $(document).on('click', '.removeConcert', function() {
-        var concertsToGoTo= JSON.parse(localStorage.getItem('savedConcerts'));
+    $(document).on('click', '.removeConcert', function () {
+        var concertsToGoTo = JSON.parse(localStorage.getItem('savedConcerts'));
         var idIndex = $(this).attr('id')
         console.log(idIndex)
         console.log(idIndex.charAt(1))
         var indexOfDiv = idIndex.charAt(1)
-        $(`#${indexOfDiv}`).remove() 
+        $(`#${indexOfDiv}`).remove()
         // $("#" + indexOfDiv).remove()
-        
+
         concertsToGoTo.splice(idIndex, 1);
         savedConcerts = concertsToGoTo;
 
         localStorage.setItem("savedConcerts", JSON.stringify(concertsToGoTo));
-    
-    
-    });
-    
 
-    $(document).on('click', '#goingBack', function() {
-        
+
+    });
+
+
+    $(document).on('click', '#goingBack', function () {
+
         $('#concertInformation').hide(1000);
         $('#countryList').show(1000);
         $('#goodResults').show();
@@ -193,7 +196,7 @@ $(document).ready(function () {
     //     translateX: [
     //         {value:900, duration:10000,
     //         delay:0, elasticity:0}
-             
+
     //     ],
     //     // rotate: [
     //     //     {value:'1turn', duration:1100,
@@ -203,28 +206,32 @@ $(document).ready(function () {
     //     });
 
     // document.querySelector('.btn-animate').onclick = rotate.restart;
-    
+
     var rotate = anime({
         targets: '#favorites',
         rotate: [
-            {value:'3turn', duration:3000,
-            easing: 'easeInOutSine'}
+            {
+                value: '3turn', duration: 3000,
+                easing: 'easeInOutSine'
+            }
         ],
-        opacity:[
-            {value:1, duration:1000,
-            delay:0, elasticty:0}
+        opacity: [
+            {
+                value: 1, duration: 1000,
+                delay: 0, elasticty: 0
+            }
         ],
-            autoplay: false
-        });
+        autoplay: false
+    });
 
     document.querySelector('.fas').onclick = rotate.restart;
 
     var relativeOffset = anime.timeline({
-        autoplay:false
+        autoplay: false
     });
 
     relativeOffset
-        
+
         .add({
             targets: '#openingContainer .switch1.el',
             translateY: -1100,
@@ -235,8 +242,10 @@ $(document).ready(function () {
             targets: '#openingContainer .switch2.el',
             translateX: 700,
             opacity: [
-                {value:1,duration:200,
-                value:0}
+                {
+                    value: 1, duration: 200,
+                    value: 0
+                }
             ],
             easing: 'easeOutExpo',
             offset: '-=600', // Starts 600ms before the previous animation ends
@@ -244,7 +253,7 @@ $(document).ready(function () {
         .add({
             targets: '#openingContainer .switch4.el',
             translateY: 700,
-            
+
             easing: 'easeOutExpo',
             offset: '-=600', // Starts 800ms before the previous animation ends    
         })
@@ -254,68 +263,78 @@ $(document).ready(function () {
             easing: 'easeOutExpo',
             offset: '-=500', // Starts 800ms before the previous animation ends 
         })
-        
-        document.querySelector('.rotateBoxes').onclick = relativeOffset.restart;
 
-        $(document).on('click', '.rotateBoxes', function() {
-            $('.nav-wrapper').delay(1800).slideDown(300);
-            $('.switch1').removeClass('animated bounceInLeft ')
-            $('.switch2').removeClass('animated bounceInDown')
-            $('.switch3').removeClass('animated bounceInUp')
-            $('.switch4').removeClass('animated bounceInRight')
+    document.querySelector('.rotateBoxes').onclick = relativeOffset.restart;
 
-                var styles = {
-                    background: 'url(assets/images/concert.jpg)',
-                    backgroundRepeat: 'no-repeat',
-                    backgroundSize: 'cover',
-                    backgroundAttachment: 'fixed',
-                    backgroundPoistion: 'center',
-                    margin: 0,
-                    padding: 0,
-                    zIndex:10,
-                }
-                $('body, html').css(styles) 
-            setTimeout(function(){
-                   $('.rotateBoxes').hide();
-               }, 1500);
-        })
-    
-        function begin() {
-            $('.switch1').addClass('animated bounceInLeft ')
-            $('.switch2').addClass('animated bounceInDown')
-            $('.switch3').addClass('animated bounceInUp')
-            $('.switch4').addClass('animated bounceInRight')
-            $('.rotateBoxes').addClass('animated pulse infinite') 
+    $(document).on('click', '.rotateBoxes', function () {
+        $('.nav-wrapper').delay(1800).slideDown(300);
+        $('.switch1').removeClass('animated bounceInLeft ')
+        $('.switch2').removeClass('animated bounceInDown')
+        $('.switch3').removeClass('animated bounceInUp')
+        $('.switch4').removeClass('animated bounceInRight')
+
+        var styles = {
+            background: 'url(assets/images/concert.jpg)',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundAttachment: 'fixed',
+            backgroundPoistion: 'center',
+            margin: 0,
+            padding: 0,
+            zIndex: 10,
         }
-        begin();
+        $('body, html').css(styles)
+        setTimeout(function () {
+            $('.rotateBoxes').hide();
+        }, 1500);
+    })
 
-        $(document).on('click', '.weather', function () {
-            console.log("clicked");
-            console.log(this);
-            var wLat = $(this).attr('data-latitude');
-            var wLong = $(this).attr('data-longitude');
-            var wCoords = (wLat + "," + wLong);
-            console.log(wCoords);
-            var wKey = "8c6957ee01f24957b5bd52d69928bd75";
-            var weatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + wLat + "&lon=" + wLong + "&appid=" + wKey + "&units=imperial";
-            $.ajax({
-                url: weatherURL,
-                method: "GET",
-            }).then(function (response) {
-                var vTempHigh = response.main.temp_max;
-                console.log(vTempHigh);
-                var vTempLow = response.main.temp_min;
-                console.log(vTempLow);
-                sessionStorage.setItem('highTemp', vTempHigh);
-                sessionStorage.setItem('lowTemp', vTempLow)
-            })
-            var vTempHigh = sessionStorage.getItem('highTemp');
-            var vTempLow = sessionStorage.getItem('lowTemp');
-            $(this).text("Today's Forecast: High of " + vTempHigh + " and low of " + vTempLow + " (F)");
-            console.log(this);
-            console.log("got here");
-        
-        
+    function begin() {
+        $('.switch1').addClass('animated bounceInLeft ')
+        $('.switch2').addClass('animated bounceInDown')
+        $('.switch3').addClass('animated bounceInUp')
+        $('.switch4').addClass('animated bounceInRight')
+        $('.rotateBoxes').addClass('animated pulse infinite')
+    }
+    begin();
+
+    database.ref().on("value", function(snapshot) {
+        // Log everything that's coming out of snapshot
+        console.log("Got Here");
+        $("#recentSearch").text(snapshot.val().Band);
+    }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    });
+
+    $(document).on('click', '.weather', function () {
+        console.log("clicked");
+        console.log(this);
+        var wLat = $(this).attr('data-latitude');
+        var wLong = $(this).attr('data-longitude');
+        var wCoords = (wLat + "," + wLong);
+        console.log(wCoords);
+        var wKey = "8c6957ee01f24957b5bd52d69928bd75";
+        var weatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + wLat + "&lon=" + wLong + "&appid=" + wKey + "&units=imperial";
+        $.ajax({
+            url: weatherURL,
+            method: "GET",
+        }).then(function (response) {
+            var vTempHigh = response.main.temp_max;
+            console.log(vTempHigh);
+            var vTempLow = response.main.temp_min;
+            console.log(vTempLow);
+            sessionStorage.setItem('highTemp', vTempHigh);
+            sessionStorage.setItem('lowTemp', vTempLow)
         })
+        var vTempHigh = sessionStorage.getItem('highTemp');
+        var vTempLow = sessionStorage.getItem('lowTemp');
+        setTimeout(function () {
+            $(this).text("Today's Forecast: High of " + vTempHigh + " and low of " + vTempLow + " (F)");
+        }, 500);
+        
+        
+
+    })
 
 })
+
